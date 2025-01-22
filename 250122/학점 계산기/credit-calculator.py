be776d9a4dc5a8ca124701554353cfd781559a93
Sunil Choudhury <linuschoudhury@gmail.com>
2025-01-22 17:27:04 +0530

    Title: 학점 계산기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250122/학점 계산기/credit-calculator.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=list(map(float,input().split()))
+total=sum(arr)
+avg=total/len(arr)
+print("%.1f"%avg)
+if(avg>=4.0):
+    print('Perfect')
+elif(avg>=3.0):
+    print('Good')
+else:
+    print('Poor')
