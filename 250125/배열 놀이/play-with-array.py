41d8d1541bad9de7966b4bad6d6d1682cfd2a8d7
Sunil Choudhury <linuschoudhury@gmail.com>
2025-01-25 16:58:04 +0530

    Title: 배열 놀이, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250125/배열 놀이/play-with-array.py
+++ 250125/배열 놀이/play-with-array.py
@@ -4,10 +4,14 @@ for i in range(q):
     qarr=list(map(int,input().split()))
     #print(qarr[1])
     #print(arr)
+    #print(len(arr))
     if qarr[0]==1:
         print(arr[qarr[1]-1])
     elif qarr[0]==2:
-        print(arr.index(qarr[1])+1)
+        if qarr[1] in arr:
+            print(arr.index(qarr[1])+1)
+        else:
+            print(0)
     elif qarr[0]==3:
         for i in range(qarr[1]-1,qarr[2]):
             print(arr[i],end=' ')
