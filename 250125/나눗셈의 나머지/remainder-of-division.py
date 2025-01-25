09ea223f6f7a9e51aa3ab03a2ef90bffaa906411
Sunil Choudhury <linuschoudhury@gmail.com>
2025-01-25 11:09:33 +0530

    Title: 나눗셈의 나머지, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250125/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,18 @@
+arr=input().split()
+a,b=int(arr[0]),int(arr[1])
+remarray=[]
+while a>0:
+    remarray.append(a%b)
+    a=a//b
+#print(remarray)
+
+total=0
+for i in range(10):
+    count=0
+    for j in range(len(remarray)):
+        if i==remarray[j]:
+            count+=1
+    #print(i,count)
+    total+=count*count
+print(total)
+    
