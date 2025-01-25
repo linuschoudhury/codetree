7420bf0d0e64674e24f2f8d31ecf0033d2f98160
Sunil Choudhury <linuschoudhury@gmail.com>
2025-01-25 18:06:26 +0530

    Title: 연속부분수열일까, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250125/연속부분수열일까/contiguous-array-or-not.py
+++ 250125/연속부분수열일까/contiguous-array-or-not.py
@@ -1,14 +1,12 @@
 n1,n2=map(int,input().split())
 A=list(map(int,input().split()))
 B=list(map(int,input().split()))
-intA=0
-intB=0
+strA=''
+strB=''
 for i in range(len(A)):
-    intA=intA*10+A[i]
+    strA+=str(A[i])
 for i in range(len(B)):
-    intB=intB*10+B[i]
-strA=str(intA)
-strB=str(intB)
+    strB+=str(B[i])
 if strB in strA:
     print('Yes')
 else:
