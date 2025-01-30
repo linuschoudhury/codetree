8e9a9326985fe09d923ad9294f0b6d9655d87c28
Sunil Choudhury <linuschoudhury@gmail.com>
2025-01-31 00:29:00 +0530

    Title: 특정 문자로 끝나는 문자열, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250131/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
+++ 250131/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
@@ -1,7 +1,11 @@
 arr=[]
+count=0
 for i in range(11):
     word=input()
     arr.append(word)
 for i in range(10):
     if arr[i][-1]==arr[-1]:
         print(arr[i])
+        count+=1
+if count==0:
+    print('None')
