62eada93f2151671ca07f5f66b59400f74ec4d19
Sunil Choudhury <linuschoudhury@gmail.com>
2025-02-01 01:17:50 +0530

    Title: 문자 제거하기, Time: 74ms, Memory: 19MB, Status: Runtime Error - CodeTree

---
+++ 250201/문자 제거하기/del-char.py
@@ -0,0 +1,18 @@
+try:
+    word = input()
+    while len(word) > 0:
+        word = list(word)
+        try:
+            pos = int(input())
+            if pos <= len(word):
+                word.pop(pos)
+            else:
+                word.pop(len(word)-1)
+            word = ''.join(word)
+            print(word)
+        except EOFError:
+            break
+except EOFError:
+    pass
+
+    
