b4c229a85cb4006d9e9f8244ac884525cfa0bcd7
Sunil Choudhury <linuschoudhury@gmail.com>
2025-02-01 23:19:35 +0530

    Title: 소문자와 숫자, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250202/소문자와 숫자/letter-and-number.py
+++ 250202/소문자와 숫자/letter-and-number.py
@@ -1,4 +1,4 @@
 word=input()
 for char in word:
-    if 'a'<=char<='z' or 'A'<=char<='Z' or '1'<=char<='9':
+    if 'a'<=char<='z' or 'A'<=char<='Z' or '0'<=char<='9':
         print(char.lower(),end='')
