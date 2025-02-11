2f6eaefbae538daae70e8c819b9750e3498385ff
Sunil Choudhury <linuschoudhury@gmail.com>
2025-02-11 19:01:06 +0530

    Title: 나누고 빼면서 합하기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250211/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 250211/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -6,12 +6,15 @@ def whatever(A):
     total=0
     global n,m
     j=m+1
-    while j>1:
-        if j%2!=0:
-            j-=1
+    while m>=1:
+        total+=A[m-1]
+        print(m)
+        if m%2!=0:
+            m-=1
         else:
-            j//=2
-        total+=A[j-1]
+            m//=2
+        
+        
     print(total)
         
 
