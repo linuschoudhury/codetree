957d289084f22c7c2337dd5e790efac4cfabd8cf
Sunil Choudhury <linuschoudhury@gmail.com>
2025-02-11 19:55:23 +0530

    Title: 특정 구간의 합, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250211/특정 구간의 합/sum-of-specific-intervals.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+queries = [tuple(map(int, input().split())) for _ in range(m)]
+
+# Write your code here!
+for i in range(len(queries)):
+    total=0
+    for j in range (queries[i][0]-1,queries[i][1]):
+        total+=arr[j]
+    print(total)
+
+    
