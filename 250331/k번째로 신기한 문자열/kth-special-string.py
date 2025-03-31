7ffe8f3ad7d21a2d6d8ef827b8af599a32ab1cee
Sunil Choudhury <linuschoudhury@gmail.com>
2025-03-31 16:22:05 +0530

    Title: k번째로 신기한 문자열, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 250331/k번째로 신기한 문자열/kth-special-string.py
+++ 250331/k번째로 신기한 문자열/kth-special-string.py
@@ -6,9 +6,10 @@ arraylength=int(arr1[0])
 arr2=[]
 for i in range(arraylength):
     word=input()
-    if arr1[2] in word:
+    if word.startswith(arr1[2]):
         arr2.append(word)
 arr2.sort()
+#print(arr2)
 print(arr2[int(arr1[1])-1])
 
  
