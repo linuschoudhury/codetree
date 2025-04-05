10c99976effef45d6a2d499e73888cd1b531891e
Sunil Choudhury <linuschoudhury@gmail.com>
2025-04-05 12:37:24 +0530

    Title: 007, Time: 40ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250405/007/007.py
@@ -0,0 +1,13 @@
+secret_code, meeting_point, time = input().split()
+time = int(time)
+
+# Please write your code here.
+class Ramen:
+    def __init__(self,secret_code,meeting_point,time):
+        self.secret_code=secret_code
+        self.meeting_point=meeting_point
+        self.time=time
+ramen1=Ramen('codetree','L',13)
+print("secret code :",ramen1.secret_code)
+print("meeting point :",ramen1.meeting_point)
+print("time :",ramen1.time)
