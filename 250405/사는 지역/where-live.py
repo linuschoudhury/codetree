6a8f7c02d3a86d0ca1a922b8045add8689224047
Sunil Choudhury <linuschoudhury@gmail.com>
2025-04-05 15:52:53 +0530

    Title: 사는 지역, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250405/사는 지역/where-live.py
@@ -0,0 +1,27 @@
+n = int(input())
+name = []
+street_address = []
+region = []
+
+for _ in range(n):
+    n_i, s_i, r_i = input().split()
+    name.append(n_i)
+    street_address.append(s_i)
+    region.append(r_i)
+
+# Please write your code here. 
+class Detail:
+    def __init__(self,name,street_address,region):
+        self.name=name
+        self.street_address=street_address
+        self.region=region
+    def __str__(self):
+        return f"{self.name},{self.street_address},{self.region}"
+details=[]
+for i in range(n):
+    details_obj=Detail(name[i],street_address[i],region[i])
+    details.append(details_obj)
+sorted_details=sorted(details,key=lambda x:x.name)
+print('name', details[-1].name)
+print('addr', details[-1].street_address)
+print('city', details[-1].region)
