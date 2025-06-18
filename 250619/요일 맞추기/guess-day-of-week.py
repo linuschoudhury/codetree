c2a62f102e88b909a26276deb4795a2364f38b08
Sunil Choudhury <linuschoudhury@gmail.com>
2025-06-18 22:35:40 +0530

    Title: 요일 맞추기, Time: 49ms, Memory: 20MB, Status: Passed - Codetree

--- 250619/요일 맞추기/guess-day-of-week.py
+++ 250619/요일 맞추기/guess-day-of-week.py
@@ -4,15 +4,17 @@ m1, d1, m2, d2 = map(int, input().split())
 
 numberofdays=[0,31,28,31,30,31,30,31,31,30,31,30,31]
 days=0
-
-for i in range (m1,m2):
-    days+=numberofdays[i]
-    
-
-daysfromdate=d2-d1
-days+=daysfromdate
-print(days)
+if m1<=m2:
+    for i in range (m1,m2):
+        #print(numberofdays[i])
+        days+=numberofdays[i]
+else:
+    for i in range (m2,m1):
+        #print(numberofdays[i])
+        days-=numberofdays[i]   
+days+=d2-d1
+#print(days)
 weekdays=['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 days=days%7
-print(days)
+#print(days)
 print(weekdays[days])
