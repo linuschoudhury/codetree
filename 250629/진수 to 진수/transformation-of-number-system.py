616ad3be43c9b838e93a3860ad98bd8a486c619e
Sunil Choudhury <linuschoudhury@gmail.com>
2025-06-29 16:26:36 +0530

    Title: 진수 to 진수, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250629/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,21 @@
+a, b = map(int, input().split())
+n = input()
+
+# Please write your code here.
+n=int(n)
+total=0
+i=0
+while n>0:
+    c=n%10
+    total+=c*(a**i)
+    #print(c,a**i,c*(a**i))
+    i+=1
+    n=n//10
+    
+#print(total)
+fintotal=[]
+while total>0:
+    c=total%2
+    fintotal.append(str(c))
+    total=total//2
+print(''.join(reversed(fintotal)))
