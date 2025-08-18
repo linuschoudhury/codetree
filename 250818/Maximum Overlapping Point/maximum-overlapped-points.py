2f49c4ff6ae5957a9074d995769243a219d3e78b
Sunil Choudhury <linuschoudhury@gmail.com>
2025-08-18 14:12:29 +0530

    Title: Maximum Overlapping Point, Time: 59ms, Memory: 16MB, Status: Passed - Codetree

--- 250818/Maximum Overlapping Point/maximum-overlapped-points.py
+++ 250818/Maximum Overlapping Point/maximum-overlapped-points.py
@@ -2,7 +2,7 @@ n = int(input())
 segments = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-arr=[0]*202
+arr=[0]*101
 for i in range(n):
     for j in range(segments[i][0],segments[i][1]+1):
         arr[j]+=1
