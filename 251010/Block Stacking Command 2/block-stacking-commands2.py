7c400db3fdba7f5a8b0658727275eeb70da86b15
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-10 12:39:19 +0530

    Title: Block Stacking Command 2, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251010/Block Stacking Command 2/block-stacking-commands2.py
+++ 251010/Block Stacking Command 2/block-stacking-commands2.py
@@ -3,9 +3,7 @@ commands = [tuple(map(int, input().split())) for _ in range(k)]
 
 # Please write your code here.
 cells=[0]*n
-
 for i in range(k):
-    for j in range(commands[i][0],commands[i][1]+1):
-        
+    for j in range(commands[i][0],commands[i][1]+1):      
         cells[j-1]+=1
 print(max(cells))
