1cd9ad6b7dcb32728722e81cf0642a997af27e87
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-11 23:42:39 +0530

    Title: Maximum Overlapping Interval, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251011/Maximum Overlapping Interval/maximum-overlapped-segments.py
@@ -0,0 +1,9 @@
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+arr=[0]*n*2
+for i in range(n):
+    for j in range(segments[i][0], segments[i][1]):
+        arr[j]+=1
+print(max(arr))
