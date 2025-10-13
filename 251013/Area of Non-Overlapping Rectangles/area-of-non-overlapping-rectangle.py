18e12519e8fc02feb34021b9ed4c10fbc8cea79f
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-13 23:46:23 +0530

    Title: Area of Non-Overlapping Rectangles, Time: 1042ms, Memory: 200MB, Status: Time Limit Exceed - Codetree

--- 251013/Area of Non-Overlapping Rectangles/area-of-non-overlapping-rectangle.py
+++ 251013/Area of Non-Overlapping Rectangles/area-of-non-overlapping-rectangle.py
@@ -8,33 +8,15 @@ x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
 x1[2], y1[2], x2[2], y2[2] = map(int, input().split())
 
 # Please write your code here.
-A={}
+rects=[]
+for i in range(3):
+    s=set()
+    for j in range(x1[i],x2[i]):
+        for k in range(y1[i],y2[i]):
+            s.add((j,k))
+    rects.append(s)
 
-for j in range(x1[0],x2[0]):
-    for k in range(y1[0],y2[0]):
-        
-        t=tuple((j,k))
-       
-        if t not in A:
-            A[t]=1
-        else:
-            A[t]+=1
-B={}
-for j in range(x1[1],x2[1]):
-    for k in range(y1[1],y2[1]):
-        t=tuple((j,k))
-        if t not in B:
-            B[t]=1
-        else:
-            B[t]+=1
-C={}
-for j in range(x1[2],x2[2]):
-    for k in range(y1[2],y2[2]):
-        t=tuple((j,k))
-        if t not in C:
-            C[t]=1
-        else:
-            C[t]+=1
-onlyA=len([k for k in A if k not in C])
-onlyB=len([k for k in B if k not in C])
-print(onlyA+onlyB)
+a,b,m=rects
+onlyA=a-m
+onlyB=b-m
+print(len(onlyA)+len(onlyB))
