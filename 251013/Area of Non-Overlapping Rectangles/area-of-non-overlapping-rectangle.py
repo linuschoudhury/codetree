dd75c3682c9eba5083d2edeb1b69c3496bc228bc
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-13 23:49:03 +0530

    Title: Area of Non-Overlapping Rectangles, Time: 1084ms, Memory: 200MB, Status: Time Limit Exceed - Codetree

--- 251013/Area of Non-Overlapping Rectangles/area-of-non-overlapping-rectangle.py
+++ 251013/Area of Non-Overlapping Rectangles/area-of-non-overlapping-rectangle.py
@@ -8,11 +8,12 @@ x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
 x1[2], y1[2], x2[2], y2[2] = map(int, input().split())
 
 # Please write your code here.
+offset=1000
 rects=[]
 for i in range(3):
     s=set()
-    for j in range(x1[i],x2[i]):
-        for k in range(y1[i],y2[i]):
+    for j in range(x1[i]+offset,x2[i]+offset):
+        for k in range(y1[i]+offset,y2[i]+offset):
             s.add((j,k))
     rects.append(s)
 
