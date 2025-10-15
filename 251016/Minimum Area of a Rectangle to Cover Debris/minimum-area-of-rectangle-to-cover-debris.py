26a814ac488336a674f8f296e27d7c6bd5516405
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-16 00:45:11 +0530

    Title: Minimum Area of a Rectangle to Cover Debris, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251016/Minimum Area of a Rectangle to Cover Debris/minimum-area-of-rectangle-to-cover-debris.py
+++ 251016/Minimum Area of a Rectangle to Cover Debris/minimum-area-of-rectangle-to-cover-debris.py
@@ -2,30 +2,20 @@ x1, y1, x2, y2 = [0] * 2, [0] * 2, [0] * 2, [0] * 2
 x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
 x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
 
-# Please write your code here.
-offset=1000
-A=set()
-x1[0], y1[0], x2[0], y2[0]=x1[0]+offset, y1[0]+offset, x2[0]+offset, y2[0]+offset
-x1[1], y1[1], x2[1], y2[1]=x1[1]+offset, y1[1]+offset, x2[1]+offset, y2[1]+offset
-for i in range(min(x1[0],x2[0]),max(x1[0],x2[0])):
-    for j in range(min(y1[0],y2[0]),max(y1[0],y2[0])):
-        A.add((i,j))
+# Check if there's any intersection
+intersect_x1 = max(x1[0], x1[1])
+intersect_y1 = max(y1[0], y1[1])
+intersect_x2 = min(x2[0], x2[1])
+intersect_y2 = min(y2[0], y2[1])
 
-for i in range(min(x1[1],x2[1]),max(x1[1],x2[1])):
-    for j in range(min(y1[1],y2[1]),max(y1[1],y2[1])):
-        A.discard((i,j))
-if A:
-    x_coords = [point[0] for point in A]
-    y_coords = [point[1] for point in A]
-    x_max = max(x_coords) + 1
-    x_min = min(x_coords)
-
-    y_max = max(y_coords)+1
-    y_min = min(y_coords)
-    print((x_max-x_min)*(y_max-y_min))
+if intersect_x1 >= intersect_x2 or intersect_y1 >= intersect_y2:
+    print((x2[0] - x1[0]) * (y2[0] - y1[0]))
 else:
-    print(0)
-
+    # There is intersection - check if rectangle 2 completely covers rectangle 1
+    if x1[1] <= x1[0] and x2[1] >= x2[0] and y1[1] <= y1[0] and y2[1] >= y2[0]:
+        print(0)
+    else:
+        print((x2[0] - x1[0]) * (y2[0] - y1[0]))
 
 
 
