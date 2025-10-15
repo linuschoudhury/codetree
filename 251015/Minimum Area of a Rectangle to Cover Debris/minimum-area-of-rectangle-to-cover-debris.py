ddf886d24333cd77004cfc1ac34f4231159f01ce
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-15 22:05:13 +0530

    Title: Minimum Area of a Rectangle to Cover Debris, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251015/Minimum Area of a Rectangle to Cover Debris/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,6 @@
+x1, y1, x2, y2 = [0] * 2, [0] * 2, [0] * 2, [0] * 2
+x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
+x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
+
+# Please write your code here.
+print((x2[0]-x1[0])*(y2[0]-y1[0]))
