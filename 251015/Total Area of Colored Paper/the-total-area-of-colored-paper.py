df968f085795efdd6d047fe87301fdf78a34eb13
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-15 20:57:31 +0530

    Title: Total Area of Colored Paper, Time: 60ms, Memory: 18MB, Status: Passed - Codetree

--- 251015/Total Area of Colored Paper/the-total-area-of-colored-paper.py
+++ 251015/Total Area of Colored Paper/the-total-area-of-colored-paper.py
@@ -4,6 +4,7 @@ x, y = zip(*points)
 x, y = list(x), list(y)
 
 # Please write your code here.
+offset=100
 x2=[0]*n
 y2=[0]*n
 for i in range(n):
@@ -11,10 +12,10 @@ for i in range(n):
     y2[i]=y[i]+8
 ymax = max(y)+max(y2)
 xmax = max(x)+max(x2)
-checked=[[0]*108 for _ in range(108)]
+checked=[[0]*208 for _ in range(208)]
 for i in range(n):
-    for j in range(x[i],x2[i]):
-        for k in range(y[i],y2[i]):
+    for j in range(x[i]+offset,x2[i]+offset):
+        for k in range(y[i]+offset,y2[i]+offset):
             checked[j][k]=1
             #print(j,k)
 print(sum(sum(row) for row in checked))
