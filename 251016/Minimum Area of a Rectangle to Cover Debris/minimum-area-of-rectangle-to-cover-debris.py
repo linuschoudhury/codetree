414ee37f4a0e8a19756949123f3fd02e703dd04b
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-16 12:35:37 +0530

    Title: Minimum Area of a Rectangle to Cover Debris, Time: 103ms, Memory: 47MB, Status: Wrong Answer - Codetree

--- 251016/Minimum Area of a Rectangle to Cover Debris/minimum-area-of-rectangle-to-cover-debris.py
+++ 251016/Minimum Area of a Rectangle to Cover Debris/minimum-area-of-rectangle-to-cover-debris.py
@@ -4,27 +4,30 @@ x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
 
 # Please write your code here.
 offset=1000
+checked = [[0] * 2001 for _ in range(2001)]
 A=set()
-x1[0], y1[0], x2[0], y2[0]=x1[0]+offset, y1[0]+offset, x2[0]+offset, y2[0]+offset
-x1[1], y1[1], x2[1], y2[1]=x1[1]+offset, y1[1]+offset, x2[1]+offset, y2[1]+offset
-for i in range(min(x1[0],x2[0]),max(x1[0],x2[0])):
-    for j in range(min(y1[0],y2[0]),max(y1[0],y2[0])):
-        A.add((i,j))
+x1_0, y1_0, x2_0, y2_0=x1[0]+offset, y1[0]+offset, x2[0]+offset, y2[0]+offset
+x1_1, y1_1, x2_1, y2_1=x1[1]+offset, y1[1]+offset, x2[1]+offset, y2[1]+offset
+del x1,x2,y1,y2
+for i in range(min(x1_0,x2_0),max(x1_0,x2_0)):
+    for j in range(min(y1_0,y2_0),max(y1_0,y2_0)):
+        checked[i][j]=1
 
-for i in range(min(x1[1],x2[1]),max(x1[1],x2[1])):
-    for j in range(min(y1[1],y2[1]),max(y1[1],y2[1])):
-        A.discard((i,j))
-if A:
-    #x_coords = max([point[0] for point in A])
-    #y_coords = max([point[1] for point in A])
-    x_max = max([point[0] for point in A]) + 1
-    x_min = min([point[0] for point in A])
+for i in range(min(x1_1,x2_1),max(x1_1,x2_1)):
+    for j in range(min(y1_1,y2_1),max(y1_1,y2_1)):
+        checked[i][j]=2
+min_x,min_y=2001,2001
+max_x,max_y=0,0
+for i in range(len(checked)):
+    for j in range(len(checked[i])):
+        if checked[i][j]==1:
+            min_x=min(i,min_x)
+            max_x=max(i,max_x)
+            min_y=min(j,min_y)
+            max_y=max(j,max_y)
 
-    y_max = max([point[1] for point in A])+1
-    y_min = min([point[1] for point in A])
-    print((x_max-x_min)*(y_max-y_min))
-else:
-    print(0)
+print(((max_x-min_x)+1)*((max_y-min_y)+1))
+
 
 
 
