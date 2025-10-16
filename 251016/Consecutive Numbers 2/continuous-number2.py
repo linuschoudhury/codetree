cdba9ad86e48a3a4e9966cae40cf1a11cbc14529
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-16 14:26:14 +0530

    Title: Consecutive Numbers 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251016/Consecutive Numbers 2/continuous-number2.py
+++ 251016/Consecutive Numbers 2/continuous-number2.py
@@ -3,13 +3,12 @@ arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
 count=1
-maxcount=0
+maxcount=1
 for i in range(n-1):
     if arr[i]==arr[i+1]:
         count+=1
     else:
         maxcount=max(count,maxcount)
         count=1
+
 print(maxcount)
-
-
