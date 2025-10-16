aea52cd6aef458926a888042169ccc3a11a3810c
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-16 21:15:44 +0530

    Title: Who Pays the Fine, Time: 80ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251016/Who Pays the Fine/who-will-pay.py
@@ -0,0 +1,14 @@
+N, M, K = map(int, input().split())
+student = [int(input()) for _ in range(M)]
+
+# Please write your code here.
+A=[0]*(N+1)
+for i in range(len(student)):
+    A[student[i]]+=1
+    if A[student[i]]==K:
+        break
+print(student[i])
+
+
+
+    
