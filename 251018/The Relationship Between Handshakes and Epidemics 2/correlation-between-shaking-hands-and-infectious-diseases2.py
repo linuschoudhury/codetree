e1cb45fc05e82463a9f086677ea24fdc4731dfb7
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-18 03:02:04 +0530

    Title: The Relationship Between Handshakes and Epidemics 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251018/The Relationship Between Handshakes and Epidemics 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 251018/The Relationship Between Handshakes and Epidemics 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -7,15 +7,20 @@ infected=[]
 infected.append(P)
 k=[0]*(N+1)
 for i in range(T):
-    if handshakes[i][1] in infected and k[handshakes[i][1]]<K:
+    if handshakes[i][1] in infected and handshakes[i][2] in infected:
+        k[handshakes[i][1]]+=1
+        k[handshakes[i][2]]+=1
+    elif handshakes[i][1] in infected and k[handshakes[i][1]]<K:
         infected.append(handshakes[i][2])
         k[handshakes[i][1]]+=1
+        #print(k[handshakes[i][1]])
         #print(handshakes[i][1])
     elif handshakes[i][2] in infected and k[handshakes[i][2]]<K:
         infected.append(handshakes[i][1])
         k[handshakes[i][2]]+=1
+        #print(k[handshakes[i][2]])
         #print(handshakes[i][2])
-
+#print(infected)
 answer=[0]*N
 for i in range(N):
     if i+1 in infected:
