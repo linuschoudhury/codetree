515d030751b54ac841218354d6075fd72261b434
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-19 23:46:24 +0530

    Title: Movement of a Small Marble, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251019/Movement of a Small Marble/small-marble-movement.py
+++ 251019/Movement of a Small Marble/small-marble-movement.py
@@ -3,25 +3,16 @@ r, c, d = input().split()
 r, c = int(r), int(c)
 
 # Please write your code here.
-D = {'R':0, 'D':1, 'L':2, 'U':3}
-d_num = D[d]
-
-dr = [0, 1, 0, -1]  # row changes
-dc = [1, 0, -1, 0]  # column changes
-
-for _ in range(t+1):
-    r += dr[d_num]
-    c += dc[d_num]
-
-    # bounce off walls
-    if r == 0 and d_num == 3:  # U → D
-        d_num = 1
-    elif r == n-1 and d_num == 1:  # D → U
-        d_num = 3
-
-    if c == 0 and d_num == 2:  # L → R
-        d_num = 0
-    elif c == n-1 and d_num == 0:  # R → L
-        d_num = 2
-
-print(r, c)
+D={'R':0,'D':1,'U':2,'L':3}
+d_num=D[d]
+dc=[1,0,0,-1]
+dr=[0,1,-1,0]
+for i in range(t):
+    nr=r+dr[d_num]
+    nc=c+dc[d_num]
+    if nc<1 or nc>n or nr<1 or nr>n:
+        d_num=3-d_num
+    else:
+        r=r+dr[d_num]
+        c=c+dc[d_num]
+print(r,c)
