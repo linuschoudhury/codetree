58663651c5379a3e9bcecf088a504d8bdc254b50
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-22 14:13:27 +0530

    Title: On the Checkerboard 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251022/On the Checkerboard 2/on-the-checkboard-2.py
+++ 251022/On the Checkerboard 2/on-the-checkboard-2.py
@@ -4,8 +4,8 @@ grid = [list(input().split()) for _ in range(R)]
 # Please write your code here.
 currx=curry=0
 count=0
-for i in range(0,R):
-    for j in range(0,C):
+for i in range(1,R):
+    for j in range(1,C):
         for k in range(i+1,R-1):
             for l in range(j+1,C-1):
                 if grid[0][0]!=grid[i][j] and grid[i][j]!=grid[k][l] and grid[k][l]!=grid[R-1][C-1]:
