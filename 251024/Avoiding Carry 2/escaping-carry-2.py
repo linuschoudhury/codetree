c0ba3585eaa08f36ae490ebf94bdb7f312ef8f53
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-24 02:31:26 +0530

    Title: Avoiding Carry 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251024/Avoiding Carry 2/escaping-carry-2.py
+++ 251024/Avoiding Carry 2/escaping-carry-2.py
@@ -15,7 +15,10 @@ for i in range(n):
         for k in range(j+1,n):
             if (nums[i][4]+nums[j][4]+nums[k][4])<10 and (nums[i][3]+nums[j][3]+nums[k][3])<10 and (nums[i][2]+nums[j][2]+nums[k][2])<10 and (nums[i][1]+nums[j][1]+nums[k][1])<10 and (nums[i][0]+nums[j][0]+nums[k][0])<10:
                 sums.append(arr[i]+arr[j]+arr[k])
-print(max(sums))
+if sums:
+    print(max(sums))
+else:
+    print(-1)
 
 
 
