1e8714414e1c19b047b07ba7e9ef8d2b56049dc3
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-24 02:30:22 +0530

    Title: Avoiding Carry 2, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251024/Avoiding Carry 2/escaping-carry-2.py
+++ 251024/Avoiding Carry 2/escaping-carry-2.py
@@ -9,13 +9,13 @@ for i in range(n):
         num=temp%10
         temp=temp//10
         nums[i][4-j]=num
-sums=0
+sums=[]
 for i in range(n):
     for j in range(i+1,n):
         for k in range(j+1,n):
             if (nums[i][4]+nums[j][4]+nums[k][4])<10 and (nums[i][3]+nums[j][3]+nums[k][3])<10 and (nums[i][2]+nums[j][2]+nums[k][2])<10 and (nums[i][1]+nums[j][1]+nums[k][1])<10 and (nums[i][0]+nums[j][0]+nums[k][0])<10:
-                sums=arr[i]+arr[j]+arr[k]
-print(sums)
+                sums.append(arr[i]+arr[j]+arr[k])
+print(max(sums))
 
 
 
