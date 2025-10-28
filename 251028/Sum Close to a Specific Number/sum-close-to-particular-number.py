2e5ae57f1f4a6810d5b937b33116be74724135ce
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-28 20:16:14 +0530

    Title: Sum Close to a Specific Number, Time: 68ms, Memory: 17MB, Status: Passed - Codetree

--- 251028/Sum Close to a Specific Number/sum-close-to-particular-number.py
+++ 251028/Sum Close to a Specific Number/sum-close-to-particular-number.py
@@ -1,13 +1,22 @@
 N, S = map(int, input().split())
 arr = list(map(int, input().split()))
 
-# Please write your code here.
-total=[0]*N
+# Please write your code here
+fucks=[]
 for i in range(N):
-    total[i]=sum(arr[i:i+4])
-    total[i]=abs(total[i]-S)
-print(min(total))
+    for j in range(N):
+        total=0
+        if i!=j:
+            total=sum(arr)-arr[i]-arr[j]
+            total=abs(total-S)
+            fucks.append(total)
+        
+print(min(fucks))
 
+        
+
+
+    
     
 
 
