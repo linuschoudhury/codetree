e0a80f2e40bd1ef1235d03b145d0fceeafc4b1d5
Sunil Choudhury <linuschoudhury@gmail.com>
2025-10-28 16:03:03 +0530

    Title: Best 13 Position 2, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 251028/Best 13 Position 2/best-place-of-13-2.py
+++ 251028/Best 13 Position 2/best-place-of-13-2.py
@@ -3,14 +3,14 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 max1=max2=0
-def overlap(a,b,c,d):
-    return a==c and b==d
+
 for i in range(n):
     for j in range(n-2):
         total1= arr[i][j]+arr[i][j+1]+arr[i][j+2]
         for k in range(n):
             for l in range(n-2):
-                if not overlap(i,j,k,l):
+                overlaps = (i == k) and (l <= j+2) and (l+2 >= j)
+                if not overlaps:
                     total2=arr[k][l]+arr[k][l+1]+arr[k][l+2]
                     max1=max(max1,total1+total2)
 print(max1)
