5e110e5ecd116e0be70d6930f4eceb0f16812290
Sunil Choudhury <linuschoudhury@gmail.com>
2025-12-01 19:38:36 +0530

    Title: Implement Radix Sort, Time: 84ms, Memory: 22MB, Status: Wrong Answer - Codetree

--- 251201/Implement Radix Sort/implement-radix-sort.py
+++ 251201/Implement Radix Sort/implement-radix-sort.py
@@ -1,17 +1,19 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-# Please write your code here.
-k=10
-for pos in range(k-1,-1,-1):
-    arr_new=[[] for _ in range(10)]
+# Corrected Radix Sort
+k = 10
+for pos in range(k-1, -1, -1):
+    arr_new = [[] for _ in range(10)]
     for i in range(len(arr)):
-        digit=(arr[i]//(10**pos))%10
+        digit = (arr[i] // (10**pos)) % 10
         arr_new[digit].append(arr[i])
-store_arr=[]
-for i in range(10):
-    for j in range(len(arr_new[i])):
-        store_arr.append((arr_new[i][j]))
-        
-for num in store_arr:
+    
+    # FIX: Update arr after each pass through this digit position
+    arr = []
+    for i in range(10):
+        for j in range(len(arr_new[i])):
+            arr.append(arr_new[i][j])
+
+for num in arr:
     print(num, end=' ')
