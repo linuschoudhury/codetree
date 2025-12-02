3582a2086e22d1e442e89e30205dc91c8ef6f99e
Sunil Choudhury <linuschoudhury@gmail.com>
2025-12-02 06:29:48 +0530

    Title: Implement Merge Sort, Time: 125ms, Memory: 23MB, Status: Passed - Codetree

---
+++ 251202/Implement Merge Sort/implement-merge-sort.py
@@ -0,0 +1,36 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+def mergesort(arr,low,high):
+    if low<high:
+        mid=(low+high)//2
+        mergesort(arr,low,mid)
+        mergesort(arr,mid+1,high)
+        merge(arr,low,mid,high)
+
+def merge(arr,low,mid,high):
+    left=arr[low:mid+1]
+    right=arr[mid+1:high+1]
+    i=0
+    j=0
+    k=low
+    while i<len(left) and j<len(right):
+        if left[i]<=right[j]:
+            arr[k]=left[i]
+            i+=1
+            k+=1
+        else:
+            arr[k]=right[j]
+            j+=1
+            k+=1
+    while i<len(left):
+        arr[k]=left[i]
+        i+=1
+        k+=1
+    while j<len(right):
+        arr[k]=right[j]
+        j+=1
+        k+=1
+mergesort(arr,0,n-1)
+print(' '.join(map(str,arr)))
