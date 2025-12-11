2f1d364b91c81518abfc67d271e50cd347994ce2
Sunil Choudhury <linuschoudhury@gmail.com>
2025-12-11 05:53:36 +0530

    Title: Determining the Validity of Parenthesis Strings, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251211/Determining the Validity of Parenthesis Strings/parentheses-string.py
+++ 251211/Determining the Validity of Parenthesis Strings/parentheses-string.py
@@ -2,17 +2,19 @@ str = input()
 
 # Please write your code here.
 s=[]
-
+valid=True
 for i in range(len(str)):
     if len(s)==0:
         if str[i]=='(':
             s.append(str[i])
+        else:
+            valid=False
     else:
         if str[i]=='(':
             s.append(str[i])
         else:
             s.pop()
-if len(s)==0:
+if len(s)==0 and valid==True:
     print('Yes')
 else:
     print('No')
