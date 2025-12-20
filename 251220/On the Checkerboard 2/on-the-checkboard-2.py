8823aa6946b72df83974d0053c48db369fc0cff5
Sunil Choudhury <linuschoudhury@gmail.com>
2025-12-20 08:07:19 +0530

    Title: On the Checkerboard 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251220/On the Checkerboard 2/on-the-checkboard-2.py
+++ 251220/On the Checkerboard 2/on-the-checkboard-2.py
@@ -3,11 +3,16 @@ grid = [list(input().split()) for _ in range(R)]
 
 # Please write your code here.
 cnt=0
-for i in range(R-2):
-    for j in range(C-2):
-        for k in range(i+1,R-1):
-            for l in range(j+1,C-1):
-                if grid[i][j]!=grid[k][l]:
-                    cnt+=1
-print(cnt-2)
+for i in range(1,R):
+    for j in range(1,C):
+        if grid[0][0]==grid[i][j]:
+            continue
+        for k in range(i+1,R):
+            for l in range(j+1,C):
+                if grid[i][j]==grid[k][l]:
+                    continue
+                if k<R-1 and l<C-1:
+                    if grid[k][l]!=grid[R-1][C-1]:
+                        cnt+=1
+print(cnt)
                 
