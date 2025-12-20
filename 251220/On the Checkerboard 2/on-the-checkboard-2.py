2b6628a021174b9faa16db299def9cf2cdd5ef70
Sunil Choudhury <linuschoudhury@gmail.com>
2025-12-20 07:35:19 +0530

    Title: On the Checkerboard 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251220/On the Checkerboard 2/on-the-checkboard-2.py
@@ -0,0 +1,13 @@
+R, C = map(int, input().split())
+grid = [list(input().split()) for _ in range(R)]
+
+# Please write your code here.
+cnt=0
+for i in range(R-1):
+    for j in range(C-1):
+        for k in range(i+1,R-1):
+            for l in range(j+1,C-1):
+                if grid[i][j]!=grid[k][l]:
+                    cnt+=1
+print(cnt-2)
+                
