b60636c8b010c77f272d6c7fb100bbbbe7f85680
Sunil Choudhury <linuschoudhury@gmail.com>
2025-12-23 04:20:57 +0530

    Title: On the Checkerboard 2, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251223/On the Checkerboard 2/on-the-checkboard-2.py
@@ -0,0 +1,22 @@
+R, C = map(int, input().split())
+grid = [list(input().split()) for _ in range(R)]
+
+# Please write your code here.
+startcolor=grid[0][0]
+endcolor=grid[R-1][C-1]
+validpaths=0
+#for first jump
+for row1 in range(1,R-2):
+    for column1 in range(1,C-2):
+        if grid[row1][column1]==startcolor:
+            continue
+        #for second jump
+        for row2 in range(row1+1,R-1):
+            for column2 in range(column1,R-2):
+                if grid[row1][column1]==grid[row2][column2]:
+                    continue
+                else:
+                    validpaths+=1
+print(validpaths)
+
+
